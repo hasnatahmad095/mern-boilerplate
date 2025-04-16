2b14fb3d4c547a887a4663103eca8c8546b73795
hasnatahmad095 <hasnat4258@gmail.com>
2025-04-16 13:23:33 +0500

    crud routes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 const PORT = 4000;
 
-const array = []
+let array = []
 
 app.get('/api/data', (req, res) => {
     res.send(array)
@@ -17,6 +17,36 @@ app.post('/api/additem' , (req, res)=> {
     array.push(req.body)
     res.send({message: "Data send Successfully" , success : true })
 })
+
+app.delete(`/delete-item/:id`, (req, res)=>{
+    var id = req.params.id
+
+    array = array.filter((e, i) => {
+        return e.id !== id
+    })
+
+    res.send({message: "Item Deleted!" , success : true })
+})
+
+
+
+app.patch(`/edit-item/:id`, (req, res)=>{
+    var id = req.params.id
+    var newObj = req.body
+
+    let selectedIndex = array.findIndex((e, i) => {
+        return e.id === id
+    })
+
+    console.log(selectedIndex, "....index");
+    
+
+    array[selectedIndex] = newObj
+
+    res.send({message: "Item Updated!" , success : true })
+})
+
+
 
 app.listen(PORT, () => {
   console.log("Server is running");
